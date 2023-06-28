9fd120161616cd87b1dbdf5a81db08576aa650dd
Christian! <67934837+EverythingWillBeCode@users.noreply.github.com>
2023-06-28 11:13:14 -0300

    Creo mi primier archivo de Python

---
+++ HolaMundo.py
@@ -0,0 +1,7 @@
+print("-------------------------------------------------------")
+print("Ejercicio de Prueba de Codigo, Saludo Hola Mundo!.")
+print("-------------------------------------------------------")
+
+#Salida
+print("Hola Mundo!!!")
+print("Punto Digital Junio 2023!!!")
